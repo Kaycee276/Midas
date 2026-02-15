4bb07d7d3cfa9b33f25d7ac55bcdbbe345cf5c1c
Kaycee276 <kizuaba@gmail.com>
2026-02-15 06:43:51 +0100

    Handled uncaught exception

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,42 @@
-require('dotenv').config();
-const app = require('./src/config/app');
-const logger = require('./src/utils/logger');
+require("dotenv").config();
+const app = require("./src/config/app");
+const logger = require("./src/utils/logger");
 
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-  console.log(`Server is listening at http://localhost:${PORT}`);
+	logger.info(`Server running on port ${PORT}`);
+	console.log(`Server is listening at http://localhost:${PORT}`);
 });
 
 // Graceful shutdown
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM received, shutting down gracefully');
-  server.close(() => {
-    logger.info('Server closed');
-    process.exit(0);
-  });
+process.on("SIGTERM", () => {
+	logger.info("SIGTERM received, shutting down gracefully");
+	server.close(() => {
+		logger.info("Server closed");
+		process.exit(0);
+	});
 });
 
-process.on('SIGINT', () => {
-  logger.info('SIGINT received, shutting down gracefully');
-  server.close(() => {
-    logger.info('Server closed');
-    process.exit(0);
-  });
+process.on("SIGINT", () => {
+	logger.info("SIGINT received, shutting down gracefully");
+	server.close(() => {
+		logger.info("Server closed");
+		process.exit(0);
+	});
 });
 
 // Handle unhandled rejections
-process.on('unhandledRejection', (err) => {
-  logger.error('Unhandled Rejection:', err);
-  server.close(() => {
-    process.exit(1);
-  });
+process.on("unhandledRejection", (err) => {
+	logger.error("Unhandled Rejection:", err);
+	server.close(() => {
+		process.exit(1);
+	});
+});
+
+process.on("uncaughtException", (err) => {
+	logger.error("Uncaught Exception:", err);
+	server.close(() => {
+		process.exit(1);
+	});
 });
